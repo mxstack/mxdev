f369f0f17ad67946f05e8060720f7ce995786130
Jens W. Klein <jk@kleinundpartner.at>
2022-06-04 16:47:05 +0200

    modern tempfile handling

--- src/mxdev/tests/fixtures.py
+++ src/mxdev/tests/fixtures.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import shutil
 import tempfile
 import pathlib
 
@@ -8,19 +7,14 @@ import pathlib
 @pytest.fixture
 def tempdir():
     cwd = os.getcwd()
-    tempdir = pathlib.Path(tempfile.mkdtemp()).resolve()
-    try:
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = pathlib.Path(tempdir).resolve()
         os.chdir(tempdir)
         try:
             yield tempdir
         finally:
             os.chdir(cwd)
-    finally:
-        try;
-            shutil.rmtree(tempdir)
-        except PermissionError:
-            # On windows this is not allowed
-            pass
+
 
 @pytest.fixture
 def src(tempdir):
