7287d6c4d9571e31bfe457b542406449d2859a89
Jens W. Klein <jk@kleinundpartner.at>
2023-12-09 18:37:12 +0100

    make tests work

--- src/mxdev/tests/test_including.py
+++ src/mxdev/tests/test_including.py
@@ -14,11 +14,26 @@ def test_resolve_dependencies_files():
     assert file_list[3].name == "file01.ini"
 
 
-def test_resolve_dependencies_http(tmp_path):
+def test_resolve_dependencies_http(tmp_path, httpretty):
     from mxdev.including import resolve_dependencies
 
     base = pathlib.Path(__file__).parent / "data"
+    with open(base / "file_with_http_include02.ini") as fio:
+        httpretty.register_uri(
+            httpretty.GET,
+            "http://www.example.com/file_with_http_include02.ini",
+            fio.read(),
+            status=200,
+        )
+    with open(base / "file_with_http_include03.ini") as fio:
+        httpretty.register_uri(
+            httpretty.GET,
+            "http://www.example.com/file_with_http_include03.ini",
+            fio.read(),
+            status=200,
+        )
     file_list = resolve_dependencies(base / "file_with_http_include01.ini", tmp_path)
+    assert len(file_list) == 4
 
 
 def test_resolve_dependencies_filenotfound(tmp_path):
@@ -27,3 +42,15 @@ def test_resolve_dependencies_filenotfound(tmp_path):
     base = pathlib.Path(__file__).parent / "data"
     with pytest.raises(FileNotFoundError):
         resolve_dependencies(base / "file__not_found.ini", tmp_path)
+
+def test_read_with_included():
+    from mxdev.including import read_with_included
+
+    base = pathlib.Path(__file__).parent / "data"
+    cfg = read_with_included(base / "file01.ini")
+    assert cfg["settings"]["test"] == "1"
+    assert cfg["settings"]["unique_1"] == "1"
+    assert cfg["settings"]["unique_2"] == "2"
+    assert cfg["settings"]["unique_3"] == "3"
+    assert cfg["settings"]["unique_4"] == "4"
+
